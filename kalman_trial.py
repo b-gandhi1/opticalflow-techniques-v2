2bffd3eeb02990750373bcc93fe47208448ac903
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-01-11 17:37:19 +0000

    kalman? what is it?

--- kalman_trial.py
+++ kalman_trial.py
@@ -5,3 +5,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 from sklearn.metrics import (accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, classification_report, roc_curve, RocCurveDisplay, auc)
 from filterpy.kalman import KalmanFilter
+
+# Initialize the filter
+kf = KalmanFilter(dim_x=2, dim_z=1)
+
+kf.F = np.eye(2) # state transition matrix
+kf.H = np.array([[1.,0.]]) # measurement function
+kf.R = np.array([[0.1]]) # measurement noise covariance matrix
+kf.Q = np.eye(2) # process noise covariance matrix
+
+kf.x = np.zeros((2,1)) # initial state estimate
+kf.P = np.eye(2) # initial state covariance matrix
+
+for val in training_data: 
+    kf.update(val)
+    kf.predict()
