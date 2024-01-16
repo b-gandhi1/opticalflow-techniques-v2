3525273bf31477fbaf4affb11e8a9ebdbfcb6360
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-01-16 18:43:01 +0000

    euler conversions successful :)

--- quat2euler.py
+++ quat2euler.py
@@ -2,19 +2,6 @@ import numpy as np
 import pandas as pd	
 # import math
 
-# load data > extract data
-fib_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope1-20-Nov-2023--14-06-58.csv', delimiter=',')
-fib_gnd_truth_df1 = fib_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-
-fib_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope2-20-Nov-2023--14-09-23.csv', delimiter=',')
-fib_gnd_truth_df2 = fib_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-
-web_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam1-20-Nov-2023--15-56-11.csv', delimiter=',')
-web_gnd_truth_df1 = web_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-
-web_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam2-20-Nov-2023--15-59-11.csv', delimiter=',')
-web_gnd_truth_df2 = web_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-
 def euler_from_quaternion(x, y, z, w):
     """
     Convert a quaternion into euler angles (roll, pitch, yaw)
@@ -43,33 +30,48 @@ def euler_from_quaternion(x, y, z, w):
 # convert data
 def convertdata(data): 
     # data = fib_gnd_truth_df1, fib_gnd_truth_df2, web_gnd_truth_df1, web_gnd_truth_df2 # one of these
-    
     rotations = pd.DataFrame(columns=['roll_x','pitch_y','yaw_z'])
-    
-    for quat in data: 
+    # print('shape of data: ',np.shape(data))
+    for _,quat in data.iterrows(): 
         
-        if type(quat) == str:
-            continue
+        # if type(quat) == str:
+        #     continue
         
-        print(quat)
-        print(type(quat))
-        w = data.iloc(quat)[0]
-        x = data.iloc(quat)[1]
-        y = data.iloc(quat)[2]
-        z = data.iloc(quat)[3]
+        # print(quat)
+        # print(type(quat))
+        w = float(quat.iloc[0])
+        x = float(quat.iloc[1])
+        y = float(quat.iloc[2])
+        z = float(quat.iloc[3])
         
-        print('quat contents: ',quat)
+        # print('quat contents: ',w,x,y,z)
+        # print(type(w),type(x),type(y),type(z))
         roll_x, pitch_y, yaw_z = euler_from_quaternion(x, y, z, w)
         
         var = pd.DataFrame({'roll_x':[roll_x],'pitch_y':[pitch_y],'yaw_z':[yaw_z]})
         rotations = pd.concat([rotations,var],ignore_index=True)
-        print('quat contents: --')
-        print(var)
+        # print('quat contents: --')
+        # print(var)
         
-        print('data type for rotations: ',type(rotations))
-        return rotations
+    # print('data type for rotations: ',type(rotations))
+    return rotations
 
 def main():
+    
+    # load data > extract data
+    fib_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope1-20-Nov-2023--14-06-58.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    fib_gnd_truth_df1 = fib_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+
+    fib_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope2-20-Nov-2023--14-09-23.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    fib_gnd_truth_df2 = fib_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+
+    web_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam1-20-Nov-2023--15-56-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    web_gnd_truth_df1 = web_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+
+    web_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam2-20-Nov-2023--15-59-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    web_gnd_truth_df2 = web_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+
+    # run conversions and save to csv
     fib1euler = convertdata(fib_gnd_truth_df1)
     fib1euler.to_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib1euler.csv')    
     fib2euler = convertdata(fib_gnd_truth_df2)
@@ -80,4 +82,4 @@ def main():
     web2euler.to_csv('data_collection_with_franka/B07LabTrials/final/webcam/web2euler.csv')
     
 if __name__ == "__main__":
-    main()
+    main() 
