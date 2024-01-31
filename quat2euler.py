6666736bd41e1a47f1b89b88c306a15e5abf4dae
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-01-31 19:52:36 +0000

    plots fine, need to rotate quaternion..

--- quat2euler.py
+++ quat2euler.py
@@ -1,31 +1,33 @@
 import numpy as np
 import pandas as pd	
 from scipy.spatial.transform import Rotation as R
+import ast
+import re
 
-# def euler_from_quaternion(x, y, z, w):
-#     """
-#     Convert a quaternion into euler angles (roll, pitch, yaw)
-#     roll is rotation around x in radians (counterclockwise)
-#     pitch is rotation around y in radians (counterclockwise)
-#     yaw is rotation around z in radians (counterclockwise)
-#     """
-#     t0 = +2.0 * (w * x + y * z)
-#     t1 = +1.0 - 2.0 * (x * x + y * y)
-#     # roll_x = math.atan2(t0, t1)
-#     roll_x = np.arctan2(t0,t1)
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    # roll_x = math.atan2(t0, t1)
+    roll_x = np.arctan2(t0,t1)
 
-#     t2 = +2.0 * (w * y - z * x)
-#     t2 = +1.0 if t2 > +1.0 else t2
-#     t2 = -1.0 if t2 < -1.0 else t2
-#     # pitch_y = math.asin(t2)
-#     pitch_y = np.arcsin(t2)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    # pitch_y = math.asin(t2)
+    pitch_y = np.arcsin(t2)
 
-#     t3 = +2.0 * (w * z + x * y)
-#     t4 = +1.0 - 2.0 * (y * y + z * z)
-#     # yaw_z = math.atan2(t3, t4)
-#     yaw_z = np.arctan2(t3,t4)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    # yaw_z = math.atan2(t3, t4)
+    yaw_z = np.arctan2(t3,t4)
 
-#     return np.rad2deg(roll_x), np.rad2deg(pitch_y), np.rad2deg(yaw_z) # not in radians
+    return roll_x, pitch_y, yaw_z # in radians
 
 def quaternion_rotation_matrix(q0,q1,q2,q3):
     """
@@ -74,18 +76,19 @@ def quaternion_rotation_matrix(q0,q1,q2,q3):
 
 # transformation matrix apply
 def transform_franka_pillow(w,x,y,z):
-    t_x, t_y, t_z = 0,68,2 # cm. Don't need these for now. Not looking at translation. 
     # trans_mat = np.array([[-1,0,0,t_x],[0,1,0,t_y],[0,0,-1,t_z],[0,0,0,1]])
     trans_mat = np.array([[-1,0,0],[0,1,0],[0,0,-1],[0,0,0]]) # use later. 
-    panda_ee = quaternion_rotation_matrix(w,x,y,z)
-    # panda_ee = R.from_quat([w,x,y,z]).as_euler('xyz', degrees=False)
     # [x',y',z'] = Rotation_matrix * [x,y,z] --- MATRIX MULTIPLICATION RULE
-    # transformed = np.matmul(trans_mat, panda_ee)
-    quat_norm = np.linalg.norm([w,x,y,z])
-    if quat_norm < 1e-300:
-        trans_euler = np.array([0,0,0])
-    else:
-        trans_euler = R.from_quat([w,x,y,z]).as_euler('xyz', degrees=False)
+    transformed = np.dot(trans_mat,np.array([w,x,y,z]))
+    # trans_euler = R.from_quat([w,x,y,z]).as_euler('xyz', degrees=False)
+    trans_euler = R.from_matrix(transformed).as_euler('xyz', degrees=False)
+    # quat_norm = np.linalg.norm([w,x,y,z])
+    # if quat_norm < 1e-300:
+    #     trans_euler = np.array([0,0,0])
+    # else:
+    #     trans_euler = R.from_quat([w,x,y,z]).as_euler('xyz', degrees=False)
+    
+    
     return trans_euler[0],trans_euler[1],trans_euler[2]
     # return transformed
 
@@ -101,18 +104,18 @@ def convertdata(data):
         
         # print(quat)
         # print(type(quat))
-        w = float(quat.iloc[0])
-        x = float(quat.iloc[1])
-        y = float(quat.iloc[2])
-        z = float(quat.iloc[3])
+        w = float(quat.iloc[6])
+        x = float(quat.iloc[3])
+        y = float(quat.iloc[4])
+        z = float(quat.iloc[5])
         
         # print('quat contents: ',w,x,y,z)
         # print(type(w),type(x),type(y),type(z))
         # trans_w,trans_x,trans_y,trans_z = transform_franka_pillow(w,x,y,z)
         # roll_x, pitch_y, yaw_z = euler_from_quaternion(trans_w,trans_x,trans_y,trans_z)
-        roll_x,pitch_y,yaw_z = transform_franka_pillow(w,x,y,z)
+        roll_x,pitch_y,yaw_z = euler_from_quaternion(x,y,z,w)
         
-        var = pd.DataFrame({'roll_x':[roll_x],'pitch_y':[pitch_y],'yaw_z':[yaw_z]})
+        var = pd.DataFrame({'roll_x':[roll_x],'pitch_y':[pitch_y],'yaw_z':[yaw_z]}).astype(float)
         rotations = pd.concat([rotations,var],ignore_index=True)
         # print('quat contents: --')
         # print(var)
@@ -123,26 +126,32 @@ def convertdata(data):
 def main():
     
     # load data > extract data
-    fib_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope1-20-Nov-2023--14-06-58.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
-    fib_gnd_truth_df1 = fib_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+    # fib_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope1-20-Nov-2023--14-06-58.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    # fib_gnd_truth_df1 = fib_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
 
-    fib_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope2-20-Nov-2023--14-09-23.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
-    fib_gnd_truth_df2 = fib_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+    # fib_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope2-20-Nov-2023--14-09-23.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    # fib_gnd_truth_df2 = fib_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
 
-    web_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam1-20-Nov-2023--15-56-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
-    web_gnd_truth_df1 = web_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+    # web_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam1-20-Nov-2023--15-56-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    # web_gnd_truth_df1 = web_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
 
-    web_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam2-20-Nov-2023--15-59-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
-    web_gnd_truth_df2 = web_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
+    # web_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam2-20-Nov-2023--15-59-11.csv', delimiter=',',dtype={'Franka a': float,'Franka bi': float,'Franka cj': float,'Franka EE jk': float},header=None)
+    # web_gnd_truth_df2 = web_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
 
+    franka1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/franka_raw/franka1_modified.csv', delimiter=',',dtype=float)
+    franka2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/franka_raw/franka2_modified.csv', delimiter=',',dtype=float)
+    franka3 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/franka_raw/franka3_modified.csv', delimiter=',',dtype=float)
+    franka4 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/franka_raw/franka4_modified.csv', delimiter=',',dtype=float)
+
+    
     # run conversions and save to csv
-    fib1euler = convertdata(fib_gnd_truth_df1)
+    fib1euler = convertdata(franka1)
     fib1euler.to_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib1euler.csv')    
-    fib2euler = convertdata(fib_gnd_truth_df2)
+    fib2euler = convertdata(franka2)
     fib2euler.to_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib2euler.csv')
-    web1euler = convertdata(web_gnd_truth_df1)
+    web1euler = convertdata(franka3)
     web1euler.to_csv('data_collection_with_franka/B07LabTrials/final/webcam/web1euler.csv')
-    web2euler = convertdata(web_gnd_truth_df2)
+    web2euler = convertdata(franka4)
     web2euler.to_csv('data_collection_with_franka/B07LabTrials/final/webcam/web2euler.csv')
     
 if __name__ == "__main__":
--- timeseries_test.py
+++ timeseries_test.py
@@ -28,58 +28,56 @@ def load_data():
     # fib_lk_bin2_raw = pickle.load(open("OF_outputs/data2_jan2023/LK_binary_fib2_2024-01-23_13-11-41.pkl", "rb"))
     
     # csv data from arm: 
-    fib_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope1-20-Nov-2023--14-06-58.csv', delimiter=',')
-    fib_gnd_truth_df1 = fib_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-    fib_df_euler1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib1euler.csv', delimiter=',',header=None,usecols=[1,2,3])
+    fib_df_euler1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib1euler.csv', delimiter=',',header=None)
     
-    fib_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fibrescope2-20-Nov-2023--14-09-23.csv', delimiter=',')
-    fib_gnd_truth_df2 = fib_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-    fib_df_euler2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib2euler.csv', delimiter=',',header=None,usecols=[1,2,3])
+    fib_df_euler2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/fibrescope/fib2euler.csv', delimiter=',',header=None)
     
-    web_df1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam1-20-Nov-2023--15-56-11.csv', delimiter=',')
-    web_gnd_truth_df1 = web_df1.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-    web_df_euler1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/web1euler.csv', delimiter=',',header=None,usecols=[1,2,3])
+    web_df_euler1 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/web1euler.csv', delimiter=',',header=None)
+    # print(np.shape(web_df_euler1))
     
-    web_df2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/webcam2-20-Nov-2023--15-59-11.csv', delimiter=',')
-    web_gnd_truth_df2 = web_df2.iloc[1:,5:] # remove first data point to match sizes, and extract quaternions
-    web_df_euler2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/web2euler.csv', delimiter=',',header=None,usecols=[1,2,3])
+    web_df_euler2 = pd.read_csv('data_collection_with_franka/B07LabTrials/final/webcam/web2euler.csv', delimiter=',',header=None)
     
-    data1 = web_gnd_truth_df1
+    data1 = web_df_euler1
     return data1
 
-def data_load_adapt(data_var):
-    max_len = max(len(d) for d in data_var)
-    padded = [np.pad(d, (0, max_len - len(d)), mode='constant') for d in data_var]
-    data_out = np.asarray(padded)
-    # data_out = data_var
-    return data_out
+# def data_load_adapt(data_var):
+#     max_len = max(len(d) for d in data_var)
+#     padded = [np.pad(d, (0, max_len - len(d)), mode='constant') for d in data_var]
+#     data_out = np.asarray(padded)
+#     # data_out = data_var
+#     return data_out
 
-def oneD_plots(timeseries):
-    plt.figure()
-    plt.plot(range(len(timeseries)),timeseries)
-    plt.tight_layout()
-    plt.show()
+# def oneD_plots(timeseries):
+#     plt.figure()
+#     plt.plot(range(len(timeseries)),timeseries)
+#     plt.tight_layout()
+#     plt.show()
 
 def main():
     data = load_data()
     # x_mat = np.asarray([data['x_val_1d'] for data in data],dtype=float) # for pickle files
-    # x,y,z = 0,1,2
-    w,x,y,z = 0,1,2,3
-    w_vals = data.iloc[0:,w]
-    x_vals = data.iloc[0:,x]
-    y_vals = data.iloc[0:,y]
-    z_vals = data.iloc[0:,z]
-    print('shape of x_mat: ', np.shape(x_vals))
+    x,y,z = 1,2,3
+    # w,x,y,z = 0,1,2,3
+    # w_vals = data.iloc[1:,w]
+    x_vals = data.iloc[1:,x]
+    x_vals = [float(x) for x in x_vals]
+    y_vals = data.iloc[1:,y]
+    y_vals = [float(y) for y in y_vals]
+    z_vals = data.iloc[1:,z]
+    z_vals = [float(z) for z in z_vals]
 
-    # oneD_plots(x_mat)
+    # oneD_plots(x_vals)
+    
     plt.figure()
-    plt.plot(range(len(w_vals)),w_vals)
-    plt.plot(range(len(x_vals)),x_vals)
+    # plt.plot(range(len(w_vals)),w_vals)
+    plt.plot(range(len(x_vals)),(x_vals))
     plt.plot(range(len(y_vals)),y_vals)
     plt.plot(range(len(z_vals)),z_vals)
-    # plt.legend(['roll_x','pitch_y','yaw_z'])
-    plt.legend(['w','x','y','z'])
-    plt.tight_layout()
+    plt.legend(['roll_x','pitch_y','yaw_z'])
+    # plt.plot(range(len(z_vals)),w_z'])
+    # plt.legend(['w','x','y','z'])
+    # plt.ylim((-1,2*np.pi))
+    # plt.tight_layout()
     plt.show()
     
 if __name__ == "__main__":
