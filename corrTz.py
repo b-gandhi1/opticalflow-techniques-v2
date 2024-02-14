6a832f8264350e311edbc361b78798068daabecc
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-02-14 20:17:38 +0000

    corrTz updates for import functions img process

--- corrTz.py
+++ corrTz.py
@@ -2,9 +2,53 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from trial_img_process_calib import webcam_process, fibrescope_process
+# from trial_img_process_calib import webcam_process, fibrescope_process
 from linkingIO import normalize_vector, statistics_calc
 brightness_z = []
+
+WIDTH = 640
+HEIGHT = 480
+
+# fibrescope image enhancement parameters: 
+CONTRAST = 3
+BRIGHTNESS = 5
+
+def fibrescope_process(frame):
+    
+    kernel = np.ones((2,2),np.uint8)
+    gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    mask_blank = np.zeros_like(gray,dtype='uint8') # ,dtype='uint8'
+    # x,y,w,h = 140,70,200,150 # after resizing frame size. UPDATE THIS
+    # x,y,w,h = 350,280,200,110 # after resizing frame size. UPDATE THIS
+    # x,y,w,h = 0,0,frame.shape[1],frame.shape[0] # no cropping, using all of it for now. 
+    # rect = cv.rectangle(mask_blank, (x, y), (x+w, y+h), (255,255,255), -1) # mask apply
+    circle = cv.circle(mask_blank, (355,345), 100, (255,255,255), -1)
+    masked = cv.bitwise_and(gray,gray,mask=circle)
+    brightened = cv.addWeighted(masked, CONTRAST, np.zeros(masked.shape, masked.dtype), 0, BRIGHTNESS)     
+    binary = cv.threshold(brightened,57,255,cv.THRESH_BINARY)[1] # might remove: + cv.thresh_otsu
+    # eroded = cv.erode(binary,np.ones((4,3),np.uint8))
+    morph_open = cv.morphologyEx(binary,cv.MORPH_OPEN,kernel)
+    morph_close = cv.morphologyEx(morph_open,cv.MORPH_CLOSE,kernel)
+    dilated = cv.dilate(morph_close,kernel)
+
+    return dilated
+    
+def webcam_process(frame):
+    # frame = cv.resize(frame,(int(width/2),int(height/2)))
+    kernel = np.ones((4,4),np.uint8)
+    gray = cv.cvtColor(frame,cv.COLOR_RGB2GRAY)
+    mask_blank = np.zeros_like(gray,dtype='uint8') # ,dtype='uint8'
+    x,y,w,h = 0,60,635,340 # (x,y) = top left params
+    rect = cv.rectangle(mask_blank, (x, y), (x+w, y+h), (255,255,255), -1) # mask apply
+    masked = cv.bitwise_and(gray,gray,mask=rect)
+    binary = cv.threshold(masked,50,255,cv.THRESH_BINARY)[1] # might remove: + cv.thresh_otsu
+    # same threshold does not work for static image and video both. Static img thresh = 50, video thresh = 140. 
+    morph_open = cv.morphologyEx(binary,cv.MORPH_OPEN,kernel)
+    morph_close = cv.morphologyEx(morph_open,cv.MORPH_CLOSE,kernel)
+    dilated = cv.dilate(morph_close,kernel)
+
+    return dilated 
+
 def get_brightness(s,path):
     cap = cv.VideoCaptute(path)
     if not cap.isOpened(): print("ERROR: Cannot open camera/video file.")
