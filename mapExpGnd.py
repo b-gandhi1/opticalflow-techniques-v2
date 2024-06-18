c48f4f188bb16df6cd4a64a67b4a0b1655abc821
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-06-18 18:19:13 +0100

    mapping updates

--- mapExpGnd.py
+++ mapExpGnd.py
@@ -6,17 +6,26 @@ import os
 import sys
 from linkingIO import normalize_vector
 
-path = sys.argv[1]
+exp_data_path = sys.argv[1]
+length = len(exp_data_path)
+pressure_sel, i = exp_data_path[:length-1], exp_data_path[length-1] # split number at the endfrom filename 
 
-dat_exp = pd.read_csv() # x
-dat_pressure = pd.read_csv() # feedback
+if pressure_sel == "pitch":
+    pressure_path = "pitch_4-jun-2024/fibrescope"+i
+elif pressure_sel == "roll":
+    pressure_path = "roll_6-jun-2024/fibrescope"+i
+else:
+    print("ERROR: Unrecognised input for pressure selector.")
+
+dat_exp = pd.read_csv("imu-fusion-outputs/LK_pitch/imu-fusion-outputs_LK_Zavg"+exp_data_path+".csv",delimiter=',',usecols=['x_vals','y_vals','z_vals'],dtype={'x_vals': float,'y_vals': float,'z_vals': float}) # x
+dat_pressure = pd.read_csv("data_collection_with_franka/B07LabTrials/imu-sensor-fusion/"+pressure_path+".csv", delimiter=',',usecols=['Pressure (kPa)'],dtype={'Pressure (kPa)': float}) # feedback
 
 dat_exp_norm = normalize_vector(dat_exp) # x normalized
 dat_pressure_norm = normalize_vector(dat_pressure) # feedback normalized
 
 dat_exp_pres_norm = pd.concat([dat_exp_norm,dat_pressure_norm],axis=1) # x with feedback (pressure)
 
-dat_gnd_euler = pd.read_csv() # y
+dat_gnd_euler = pd.read_csv("imu-fusion-outputs/LK_pitch/"+exp_data_path+"euler_gnd.csv",delimiter=',',usecols=['roll_x','pitch_y','yaw_z'],dtype={'x': float,'y': float,'z': float}) # y
 dat_gnd_euler_norm = normalize_vector(dat_gnd_euler) # y normalized
 
 # plot the data
