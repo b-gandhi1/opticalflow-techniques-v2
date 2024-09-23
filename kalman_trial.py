28a4d3f5285801f071e5de7a928029abdc59b59d
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-09-23 17:44:02 +0100

    UKF init

--- kalman_trial.py
+++ kalman_trial.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 from sklearn.metrics import (accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, classification_report, roc_curve, RocCurveDisplay, auc)
-from filterpy.kalman import KalmanFilter
+from filterpy.kalman import KalmanFilter, UnscentedKalmanFilter
+
+# const vars
+FPS = 10.0 # 10 fps
 
 def kalman_model():
     # Initialize the filter
@@ -21,6 +24,32 @@ def kalman_model():
     for val in training_data: 
         kf.update(val)
         kf.predict()
+        
+# UKF - https://filterpy.readthedocs.io/en/latest/kalman/UnscentedKalmanFilter.html 
+def fx(x, dt):
+# state transition function - predict next state based
+# on constant velocity model x = vt + x_0
+    F = np.array([[1, dt, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, dt],
+        [0, 0, 0, 1]], dtype=float)
+    return np.dot(F, x)
+def hx(x):
+    # measurement function - convert state into a measurement
+    # where measurements are [x_pos, y_pos]
+    return np.array([x[0], x[2]])
+
+def unscented_kf_model():
+    dt = 1/FPS 
+    # create sigma points to use in the filter. This is standard for Gaussian processes
+    points = MerweScaledSigmaPoints(4, alpha=.1, beta=2., kappa=-1)
+    
+    kf = kf = UnscentedKalmanFilter(dim_x=4, dim_z=2, dt=dt, fx=fx, hx=hx, points=points) # UnscentedKalmanFilter(dim_x, dim_z, dt, hx, fx, points, sqrt_fn=None, x_mean_fn=None, z_mean_fn=None, residual_x=None, residual_z=None)
+    kf.x = np.array([-1., 1., -1., 1]) # initial state
+    kf.P *= 0.2 # initial uncertainty
+    z_std = 0.1
+    kf.R = np.diag([z_std**2, z_std**2]) # 1 standard
+    kf.Q = Q_discrete_white_noise(dim=2, dt=dt, var=0.01**2, block_size=2)
 
 def main():
     data_concat = ... # pitch and roll data both, tensor format 
