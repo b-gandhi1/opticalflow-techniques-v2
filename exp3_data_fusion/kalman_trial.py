3b1b02dcd04b716feff958fdc12cbd6b32942245
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2025-05-14 18:28:34 +0100

    kalman updates plot, crashing issue

--- exp3_data_fusion/kalman_trial.py
+++ exp3_data_fusion/kalman_trial.py
@@ -30,7 +30,8 @@ class kalman_filtering:
                               [0.,0.,0.,0.,0.,0.,0.,1.]]) # state transition matrix, 8 x 8. constant velocity model.
         self.kf.H = np.eye(dim_z,dim_x) # measurement function
         self.kf.R = np.eye(dim_z)*0.1 # measurement noise covariance matrix
-        self.kf.Q = Q_discrete_white_noise(dim=2,dt=1/FPS,var=0.1) # process noise covariance matrix
+        self.kf.Q = np.eye(dim_x)*0.1 # process noise init
+        #Q_discrete_white_noise(dim=dim_z,dt=1/FPS,var=0.1) # process noise covariance matrix
 
         self.kf.x = np.zeros((dim_x,1)) # initial state estimate
         self.kf.P = np.eye(dim_x) # initial state covariance matrix
@@ -40,9 +41,9 @@ class kalman_filtering:
     # KF - https://filterpy.readthedocs.io/en/latest/kalman/KalmanFilter.html 
     # use this guide - https://medium.com/@satya15july_11937/sensor-fusion-with-kalman-filter-c648d6ec2ec2 
     
-    def get_data(self, i): # 
-        path_pitchroll = os.path.abspath(os.path.join(os.getcwd(), os.pardir,"imu-fusion-data/pitchroll_concat2"))
-    
+    def get_data(self): # 
+        # path_pitchroll = os.path.abspath(os.path.join(os.getcwd(), os.pardir,"imu-fusion-data/pitchroll_concat2"))
+        path_pitchroll = os.path.join(os.getcwd(), "imu-fusion-data/pitchroll_concat2")
         if self.mode == "pitch" or "roll": # or "tz":
             read_mode = self.mode
         else:
@@ -61,50 +62,53 @@ class kalman_filtering:
                                                                         'LKx': float, 
                                                                         'LKy': float})
         print(pitchroll_df.shape)
-        
-        # modify below
         if self.mode == "pitch":
-            pitchroll = pitchroll_df.loc(i, ['LKx', 'IMU Rx'])
-            gnd_truth = pitchroll_df.loc(i, ['Franka Ry']) 
-            return pitchroll, gnd_truth
+            pitchroll_lk = pitchroll_df.loc[:,'LKx']
+            pitchroll_gyro = pitchroll_df.loc[:,'IMU Rx']
+            gnd_truth = pitchroll_df.loc[:,'Franka Ry'] 
         elif self.mode == "roll":
-            pitchroll = pitchroll_df.loc(i, ['LKy', 'IMU Ry'])
-            gnd_truth = pitchroll_df.loc(i, ['Franka Rx'])
-            return pitchroll, gnd_truth
+            pitchroll_lk = pitchroll_df.loc[:,'LKy']
+            pitchroll_gyro = pitchroll_df.loc[:,'IMU Ry']
+            gnd_truth = pitchroll_df.loc[:,'Franka Rx']
         else:
             raise ValueError("Invalid mode. Choose 'pitch' or 'roll'. Current mode: {}".format(self.mode))
         
+        return pitchroll_lk, pitchroll_gyro, gnd_truth
+        
     def live_plot(self, x, gnd_truth, residuals):
         plt.ion()
-        plt.clf()
+        # plt.clf()
         plt.subplot(211)
         plt.title("Kalman Filter")
         plt.plot(x[0], x[1], 'ro', label='KF')
-        plt.plot(gnd_truth[0], gnd_truth[1], 'bo', label='Ground Truth')
-        plt.xlabel("X")
-        plt.ylabel("Y")
+        plt.plot(gnd_truth, 'bo', label='Ground Truth')
+        plt.xlabel("Index")
+        plt.ylabel("Rotation "+self.mode) # pitch or roll
         plt.legend()
         plt.subplot(212)
         plt.title("Residuals")
-        plt.plot(residuals[0], residuals[1], 'go', label='Residuals')
-        plt.xlabel("X")
-        plt.ylabel("Y")
+        plt.plot(residuals[0], residuals[1], 'go')#, label='Residuals')
+        plt.xlabel("Index")
+        plt.ylabel("Residuals")
         plt.legend()
         plt.pause(1/FPS)
         plt.show()
         
     def main_loop(self):
-        data = kalman_filtering.get_data()
-        print("data from get_data: ", data)
-        for i in range(len(data)):
+        lk, gyro, gnd_t = kalman_filtering.get_data(self)
+        for i in range(len(lk)):
             # u = kalman_filtering.get_mcp_data(i)
             self.kf.predict() # update x
-            self.kf.z = data[i,:-2] # update measurement, z
-            gnd_truth = data[i,-1] # ground truth
+            # update measurement, z, based on motion type
+            if self.mode == "pitch":
+                self.kf.z = np.array([[lk[i],0., gyro[i], 0.]]).T
+            if self.mode == "roll":
+                self.kf.z = np.array([[0., lk[i], gyro[i], 0.]]).T
+            gnd_truth = gnd_t[i] # ground truth
             self.kf.update(self.kf.z)
             x = self.kf.x
             residuals = self.kf.y
-            kalman_filtering.live_plot(x,gnd_truth,residuals)
+            kalman_filtering.live_plot(self, x,gnd_truth,residuals)
 
 if __name__ == "__main__":
     kf = kalman_filtering()
@@ -114,6 +118,6 @@ if __name__ == "__main__":
     except Exception as e:
         # print(f"Error in kalman_model: {e}")
         # raise e("Error in kalman_model: {e}")
-        raise SystemError(f"Error in kalman_model: {e}")
+        raise Exception(f"Error in kalman_model: {e}")
     except KeyboardInterrupt:
         raise SystemExit('KeyBoardInterrupt: Exiting the program.')
