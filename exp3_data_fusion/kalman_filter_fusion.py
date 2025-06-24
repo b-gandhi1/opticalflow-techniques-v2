62e9c5798562ea0937610b71191f4aaf9a0cb0c8
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2025-06-24 17:52:19 +0100

    monte carlo sims init

--- exp3_data_fusion/kalman_filter_fusion.py
+++ exp3_data_fusion/kalman_filter_fusion.py
@@ -4,7 +4,6 @@ import pandas as pd
 #from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 #from sklearn.metrics import (accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, classification_report, roc_curve, RocCurveDisplay, auc)
 from filterpy.kalman import KalmanFilter#, UnscentedKalmanFilter, MerweScaledSigmaPoints
-from filterpy.common import Q_discrete_white_noise
 import glob 
 import sys
 import os
@@ -34,7 +33,7 @@ class Kalman_filtering:
         self.kf.P = np.eye(dim_x) # initial state covariance matrix
         self.kf.z = np.zeros((dim_z,1)) # initial output vector, z=measurements
         self.kf._I = np.eye(dim_x)
-        
+
     # KF - https://filterpy.readthedocs.io/en/latest/kalman/KalmanFilter.html 
     # use this guide - https://medium.com/@satya15july_11937/sensor-fusion-with-kalman-filter-c648d6ec2ec2 
         
@@ -63,9 +62,11 @@ class Kalman_filtering:
         pitchroll_df = pitchroll_df.loc[pitchroll_df.ne(0).all(axis=1)].reset_index(drop=True) # remove rows with zero values
         # breakpoint()
         if self.mode == "pitch":
-            pitchroll_lk = pitchroll_df.loc[:,'LKx'] 
-            pitchroll_gyro = pitchroll_df.loc[:,'IMU Rx'] * (-1)
-            gnd_truth = pitchroll_df.loc[:,'Franka Ry'] 
+            trim_start = 600 # trim first 600 rows
+            trim_end = 3000
+            pitchroll_lk = pitchroll_df.loc[trim_start:trim_end,'LKx'] 
+            pitchroll_gyro = pitchroll_df.loc[trim_start:trim_end,'IMU Rx'] * (-1)
+            gnd_truth = pitchroll_df.loc[trim_start:trim_end,'Franka Ry'] 
             offset_gnd = -37.9
             offset_gyro = 0.0
         elif self.mode == "roll":
@@ -104,39 +105,44 @@ class Kalman_filtering:
         plt.show()
         
         return scaled_pitchroll_lk, offset_pitchroll_gyro, offset_gnd_dat
+    
+    def kf_setup(self, lk, gyro, gnd_t, window): # setup KF, get data    
+        # lk, gyro, gnd_t = self.get_data()
         
-    def main_loop(self):
-        lk, gyro, gnd_t = self.get_data()
-            
         # breakpoint()
-        start = 700 # change this to see different parts of data
-        window = start + 100
+        if window == "None": 
+            start = 0
+            end = lk.shape[0] # use all data
+        else:
+            start = 700 # change this to see different parts of data
+            end = start + window
+            
         plt.ion()
         fig = plt.figure(figsize=(6, 4))
         ax1 = fig.add_subplot(211)
-        line1, = ax1.plot([], [], 'r,', label='KF')
-        line2, = ax1.plot([], [], 'b,', label='Ground Truth')
-        ax1.set_xlim(start, window)
+        line1, = ax1.plot([], [], 'r.', label='KF')
+        line2, = ax1.plot([], [], 'b.', label='Ground Truth')
+        ax1.set_xlim(start, end)
         ax2 = fig.add_subplot(212)
-        line3, = ax2.plot([], [], 'g,', label='Residual pos')
-        line4, = ax2.plot([], [], 'm,', label='Residual vel')
-        ax2.set_xlim(start, window)
+        line3, = ax2.plot([], [], 'g.', label='Residual pos')
+        line4, = ax2.plot([], [], 'm.', label='Residual vel')
+        ax2.set_xlim(start, end)
         ax1.set_title("Kalman Filter")
         ax2.set_title("Residuals")
         ax1.set_xlabel("Index")
         ax1.set_ylabel("Rotation "+self.mode) # pitch or roll
-        ax1.set_xlim(start, window)
+        ax1.set_xlim(start, end)
         # ax1.set_ylim(-30, 30) # range for lk and gnd truth
         ax2.set_xlabel("Index")
         ax2.set_ylabel("Residuals")
-        ax2.set_xlim(start, window)
+        ax2.set_xlim(start, end)
         ax2.set_ylim(-10, 10)
         plt.tight_layout()
 
         x_store, gnd_store, residuals_store_pos, residuals_store_vel, index = [], [], [], [], []
         # index = np.linspace(start, window, num=window-start)
         
-        for i in range(start, window):
+        for i in range(start, end):
             # u = kalman_filtering.get_mcp_data(i)
             # self.kf.x, self.kf.P = 
             self.kf.predict(F=self.kf.F,Q=self.kf.Q) # update x
@@ -157,11 +163,11 @@ class Kalman_filtering:
             # x = self.kf.x
             residuals = self.kf.y
             # kalman_filtering.live_plot(self, x, gnd_truth, residuals)
-            print('x: ', self.kf.x)
+            # print('x: ', self.kf.x)
             # print('F: ', self.kf.F)
-            print('z: ', self.kf.z)
-            print('gnd_truth: ', gnd_truth)
-            print('residuals: ', residuals)
+            # print('z: ', self.kf.z)
+            # print('gnd_truth: ', gnd_truth)
+            # print('residuals: ', residuals)
             # print('z: ', self.kf.z)
             # print('x: ', self.kf.x)
             x_store = np.append(x_store, self.kf.x[x_ax])
@@ -181,12 +187,48 @@ class Kalman_filtering:
         ax2.legend()
         plt.ioff() # switch off before show
         plt.show()
+        
+        return x_store
 
+    def mc_sims_kf_loop(self): # Monte Carlo sim setup: 
+        import matplotlib
+        matplotlib.use('Agg')
+        
+        num_sims = 100 # number of simulation runs
+        print(f"Running {num_sims} Monte Carlo simulations for {self.mode} mode.")
+        lk, gyro, gnd_t = self.get_data()
+        diff_abs_sq = np.empty() # store RMSE for each simulation
+        
+        for i in range(num_sims):
+            noise_lk = np.random.normal(0, 0.1, lk.shape) + lk # add noise to LK data
+            noise_gyro = np.random.normal(0, 0.1, gyro.shape) + gyro # add noise to gyro data
+            x_pred = self.kf_setup(lk=noise_lk, gyro=noise_gyro, gnd_t=gnd_t, window="None") # run KF with noisy data
+            
+            plt.close('all') # suppress any plt fugures
+            
+            diff_abs_sq = np.append(diff_abs_sq, np.abs(x_pred - gnd_t)**2) # calculate absolute difference 
+            breakpoint()
+            print(f"Simulation {i+1}/{num_sims} completed, diff_abs_sq: {diff_abs_sq[-1]}")
+        rmse = np.sqrt(1/num_sims * np.sum(diff_abs_sq)) # calculate rmse
+        print(f"RMSE for {self.mode} mode: {rmse}")
+            
 if __name__ == "__main__":
     kf = Kalman_filtering()
     kf.mode = sys.argv[1] # input mode: "pitch" | "roll"
+    mc = sys.argv[2] if len(sys.argv) > 2 else "none" # monte carlo simulation mode
+    
     try:
-        kf.main_loop()
+        if mc == "mc":
+            print(f"Running Monte Carlo simulation for {kf.mode} mode.")
+            kf.mc_sims_kf_loop()
+        else: 
+            print(f"Running Kalman Filter for {kf.mode} mode.")
+            lk, gyro, gnd_t = kf.get_data()
+            kf.kf_setup(lk=lk, gyro=gyro, gnd_t=gnd_t, window=100)
     except KeyboardInterrupt:
         plt.close('all')
         raise SystemExit('KeyBoardInterrupt: Exiting the program.')
+
+# To run: 
+# python kalman_filter_fusion.py pitch|roll mc
+# mc - optional for monte carlo simulation. if not provided, single KF model is executed. 
